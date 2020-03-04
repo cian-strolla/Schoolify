72026cff2e0c72c67267674f0fd820c2d3276475
RonanM168 <116338731@umail.ucc.ie>
2020-03-04 11:45:02 +0000

    Fixed authentication bug

--- src/login.py
+++ src/login.py
@@ -13,7 +13,7 @@ from os import environ
 from html import escape
 
 homepage = 'teacher.py'
-result = 'error'
+result = ''
 display = ''
 
 base_page="""<body>
@@ -37,6 +37,8 @@ base_page="""<body>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>"""
+
+result = base_page
 
 cookie = SimpleCookie()
 http_cookie_header = environ.get('HTTP_COOKIE')
